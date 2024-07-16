5558252074df5f42cc14473dbc9ee1bd76f029dc
DenisRustand <denis@rustand.fr>
2024-07-16 08:27:42 +0300

    Libs

---
+++ Libraries.R
@@ -0,0 +1,20 @@
+##Libraries necessary for the course
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install(c("graph", "Rgraphviz"), dep=TRUE)
+
+#Install INLA
+install.packages("INLA",repos=c(getOption("repos"),INLA="https://inla.r-inla-download.org/R/testing"), dep=TRUE)
+
+libs <- c("R.rsp", "devtools", "sf", "survival", "ggplot2", "gridExtra",
+  "DiagrammeR", "car", "ggpubr", "spdep", "RColorBrewer", "spatstat", "sp",
+  "latticeExtra", "gridExtra", "gstat", "raster", "ggfortify", "joineR",
+  "BayesSurvival", "icenReg", "nloptr", "boot")
+
+ix <- which(!sapply(libs, require, char = TRUE))
+if (length(ix) > 0) {install.packages(libs[ix], repos = "https://cloud.r-project.org/")
+  sapply(libs[ix], require, char = TRUE)}
+
+# Install INLAjoint
+devtools::install_github('DenisRustand/INLAjoint', build_vignettes = TRUE)
